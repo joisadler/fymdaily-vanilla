56abbad281be20d26e8df270245bba2540b86fc9
Yossi Adler <joisadler@gmail.com>
2019-06-28 18:42:04 +0300

    finish add-food-page layout

--- src/javascripts/render-add-food-page.js
+++ src/javascripts/render-add-food-page.js
@@ -8,11 +8,32 @@ export default () => {
   /* eslint-disable no-param-reassign */
   const navigationButtons = [...document
     .querySelectorAll('.navigation-button')];
-  navigationButtons.forEach((button) => {
+  navigationButtons.forEach((button, i, buttons) => {
+    button.style.width = `${button.offsetHeight}px`;
+    if (i === buttons.length - 1) {
+      const foodOptionsContainer = document
+        .querySelector('.add-food-options-container');
+      foodOptionsContainer.style.height = `${button.offsetHeight * 0.3}px`;
+    }
+  });
+
+  const optionButtons = [...document
+    .querySelectorAll('.add-food-option-button')];
+  optionButtons.forEach((button) => {
     button.style.width = `${button.offsetHeight}px`;
   });
-  /* eslint-enable no-param-reassign */
 
+  const searchButton = document.querySelector('.add-food-search-button');
+  const searchBar = document.querySelector('.add-food-search-bar');
+  const addFoodHeader = document.querySelector('.add-food-header');
+  searchButton.addEventListener('click', () => {
+    searchButton.style.width = '0';
+    addFoodHeader.style.display = 'none';
+    searchBar.style.display = 'block';
+    searchBar.style.flex = '1';
+    searchBar.focus();
+  });
+  /* eslint-enable no-param-reassign */
 
   const homeButton = document.querySelector('.add-food-page-home-button');
   homeButton.addEventListener('click',
