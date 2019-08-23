7e995d546ea1da81d4ad32f5ea7fe14164688d31
Yossi Adler <joisadler@gmail.com>
2019-08-23 19:34:54 +0300

    add the hidden menu section

--- src/javascripts/listen-to-buttons.js
+++ src/javascripts/listen-to-buttons.js
@@ -1,8 +1,11 @@
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-alert */
 import renderHomePage from './render-homepage';
 import renderAddFoodPage from './render-add-food-page';
 import renderCustomFoodsPage from './render-custom-foods-page';
 import renderEatenFoodsPage from './render-eaten-foods-page';
 import renderCreateFoodPage from './render-create-food-page';
+import hiddenMenuTemplate from '../../views/hidden-menu.pug';
 
 export default () => {
   const homeButton = document.querySelector('.home-button');
@@ -13,6 +16,7 @@ export default () => {
   const cancelButton = document.querySelector('.option-cancel-button');
   const savedFoodButton = document.querySelector('.option-saved-button');
   const createFoodButton = document.querySelector('.option-create-button');
+  const hiddenMenuButton = document.querySelector('.hidden-menu-button');
 
   if (homeButton) {
     homeButton.addEventListener('click',
@@ -101,5 +105,37 @@ export default () => {
         window.history.pushState(null, null, '/custom-foods');
         renderCustomFoodsPage();
       });
+  }
+
+  if (hiddenMenuButton) {
+    const toggleHiddenMenu = async () => {
+      const data = await fetch('/user', {
+        method: 'GET',
+        credentials: 'include',
+      });
+      const { username } = await data.json();
+      const hiddenMenu = document.querySelector('.hidden-menu');
+      const currentYear = new Date().getFullYear();
+      if (hiddenMenu.classList.contains('closed')) {
+        hiddenMenu.classList.remove('closed');
+        hiddenMenu.innerHTML = hiddenMenuTemplate({
+          username,
+          currentYear,
+        });
+      } else {
+        hiddenMenu.classList.add('closed');
+        hiddenMenu.innerHTML = '';
+      }
+    };
+    hiddenMenuButton.addEventListener('click', async () => {
+      await toggleHiddenMenu();
+      const signoutButton = document.querySelector('.sign-out-button');
+      signoutButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (confirm('Do you really want to sign out?')) {
+          window.location.href = '/signout';
+        }
+      });
+    });
   }
 };
